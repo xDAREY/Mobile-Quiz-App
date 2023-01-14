adad69bdf22a86f5b6f9fd1f5daee4035c575d3a
Oluwadare Emmanuel <oluwadare.emmanuel15@gmail.com>
2023-01-15 00:15:42 +0100

    in-progress

--- app/src/main/java/com/example/mobilequizappilcation/MainActivity.java
+++ app/src/main/java/com/example/mobilequizappilcation/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.mobilequizappilcation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button playBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playBtn = findViewById(R.id.playBtn);
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
