ab67a958a4e2c24d43c11300a3fada2f92b53de0
Oluwadare Emmanuel <oluwadare.emmanuel15@gmail.com>
2023-01-16 09:26:13 +0100

    done

--- app/src/main/java/com/example/mobilequizappilcation/GameActivity.java
+++ app/src/main/java/com/example/mobilequizappilcation/GameActivity.java
@@ -2,11 +2,16 @@ package com.example.mobilequizappilcation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +25,11 @@ public class GameActivity extends AppCompatActivity {
 
     int allQuestionsAnswered;
     int questionCounter = 0;
+    int Point = 0;
+
+    ColorStateList optRbtPicked;
+    boolean answered;
+    CountDownTimer cdtimer;
 
     private QuestionsContainer onGoingQuestion;
 
@@ -44,14 +54,72 @@ public class GameActivity extends AppCompatActivity {
         optRbt3 = findViewById(R.id.answerC);
         NextBtn = findViewById(R.id.nextBtn);
 
+        optRbtPicked = optRbt1.getTextColors();
+
         inputQuestions();
         allQuestionsAnswered = questionsList.size();
         showNextQuestions();
 
+        NextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(answered == false){
+                    if(optRbt1.isChecked() || optRbt2.isChecked() || optRbt3.isChecked()){
+                        verifyAnswer();
+                        cdtimer.cancel();
+                    }else {
+                        Toast.makeText(GameActivity.this, "Choose an answer", Toast.LENGTH_SHORT).show();
+                        }
+                    }else {
+                    showNextQuestions();
+                }
+            }
+
+            private void verifyAnswer() {
+                answered = true;
+                RadioButton rbtPicked = findViewById(answerBox.getCheckedRadioButtonId());
+                int answerNo = answerBox.indexOfChild(rbtPicked) + 1;
+                if(answerNo == onGoingQuestion.getRightAnswerCounter()){
+                    Point++;
+                    QuizPoints.setText("Point: "+Point);
+                }
+                optRbt1.setTextColor(Color.YELLOW);
+                optRbt2.setTextColor(Color.YELLOW);
+                optRbt3.setTextColor(Color.YELLOW);
+
+                switch (onGoingQuestion.getRightAnswerCounter()){
+                    case 1:
+                        optRbt1.setTextColor(Color.GREEN);
+                        break;
+                    case 2:
+                        optRbt2.setTextColor(Color.GREEN);
+                        break;
+                    case 3:
+                        optRbt3.setTextColor(Color.GREEN);
+                        break;
+                }
+                if (questionCounter < allQuestionsAnswered){
+                    NextBtn.setText("NEXT");
+                }else{
+                    NextBtn.setText("Complete");
+                }
+
+            }
+        });
+
+
+
     }
 
     private void showNextQuestions() {
+
+        answerBox.clearCheck();
+        optRbt1.setTextColor(optRbtPicked);
+        optRbt2.setTextColor(optRbtPicked);
+        optRbt3.setTextColor(optRbtPicked);
+
         if(questionCounter < allQuestionsAnswered){
+            timer();
             onGoingQuestion = questionsList.get(questionCounter);
             QuizQuestion.setText(onGoingQuestion.getQuestion());
             optRbt1.setText(onGoingQuestion.getAnswerA());
@@ -59,9 +127,29 @@ public class GameActivity extends AppCompatActivity {
             optRbt3.setText(onGoingQuestion.getAnswerC());
 
             questionCounter++;
+            NextBtn.setText("SUBMIT");
+            QuizQuestionsNo.setText("Question: "+questionCounter+"/"+allQuestionsAnswered);
+            answered = false;
+
         }else{
             finish();
         }
+    }
+
+    private void timer() {
+        cdtimer = new CountDownTimer(20000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                QuizTimer.setText("00:" +1/1000);
+            }
+
+            @Override
+            public void onFinish() {
+                showNextQuestions();
+
+            }
+        }.start();
+
     }
 
     private void inputQuestions(){
